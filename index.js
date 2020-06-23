f55d3f74568b18d4a39f64db1230f1331948fba0
Mingky Kim <45954551+mingkyme@users.noreply.github.com>
2020-06-23 17:28:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const passwordFile = require('./password/password');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setActivity('존중');
 });
 
 client.on('message', message => {
     if (message.content === '존중') {
-        message.member.voiceChannel.join().then(connection => {
-            var player = connection.playFile('./music/respect.mp3');
-            player.on("end", end => {
-                message.member.voiceChannel.leave();
+        var voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            message.reply("음성 채널에 접속해주세요");
+            return;
+        }
+        var permissions = message.member.voice.channel.permissionsFor(message.client.user);
+        if(!permissions.has('CONNECT')){
+            message.reply("음성채널에 접속할 권한이 없습니다.");
+            return;
+        }
+        else if(!permissions.has('SPEAK')){
+            message.reply("말하기 권한이 없습니다.");
+            return;
+        }else{
+            voiceChannel.join().then(connection => {
+                var player = connection.play('/nodejs/DISCORD/respect-bot/music/respect.mp3');
+                player.on("finish", function(){
+                    voiceChannel.leave();
+                });
+            }).catch(err => {
+                console.log(err);
             });
-        }).catch(err => {
-            console.log(err);
-        });
+
+        }
     }
 });
 
